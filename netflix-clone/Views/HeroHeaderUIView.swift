28587860d4d57c606ba6eb7799382d17d5941932
İsmail Can Akgün <ismailcanakg1@gmail.com>
2024-04-19 17:46:46 +0300

    ana sayfaya çerçeve içerisine resim ve iki tane button eklendi onların konumları belirlendi estetik bir görünüm elde edildi

---
+++ netflix-clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  netflix-clone
+//
+//  Created by İsmail Can Akgün on 19.04.2024.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
