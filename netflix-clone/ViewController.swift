2f9a916392ea9cc27fd18e08a99681c533bc1c8d
İsmail Can Akgün <ismailcanakg1@gmail.com>
2024-04-19 15:28:38 +0300

    Initial Commit

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by İsmail Can Akgün on 19.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
