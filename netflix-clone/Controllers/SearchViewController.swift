728d4aa9e91073c9fa1fe8cb9743894aa2a3e4a0
İsmail Can Akgün <ismailcanakg1@gmail.com>
2024-04-19 17:06:32 +0300

    Tabbar görünümünü yaptım ayrıca ana sayfa dizaynını yaptım

---
+++ netflix-clone/Controllers/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  netflix-clone
+//
+//  Created by İsmail Can Akgün on 19.04.2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ netflix-clone/Views/CollectionViewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewsTableViewCell.swift
+//  netflix-clone
+//
+//  Created by İsmail Can Akgün on 19.04.2024.
+//
+
+import UIKit
+
+class CollectionViewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
